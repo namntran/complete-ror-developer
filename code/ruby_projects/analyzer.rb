d966e6909c7a1f31797b9fee0486cbafb65e31d6
namntran <namtran78@gmail.com>
2017-03-17 14:26:25 +1100

    add methods and branching if, elsif, else

--- code/ruby_projects/analyzer.rb
+++ code/ruby_projects/analyzer.rb
@@ -1,30 +1,49 @@
-puts "Please enter your first name"
+def multiply(first_number, second_number)
+  first_number.to_f * second_number.to_f
+end
 
-first_name = gets.chomp
+def divide(first_number, second_number)
+  first_number.to_f / second_number.to_f
+end
 
-puts "Please enter your last name"
+def subtract(first_number, second_number)
+  second_number.to_f - first_number.to_f
+end
 
-last_name = gets.chomp
+def mod(first_number, second_number)
+  first_number.to_f % second_number.to_f
+end
 
-puts "Welcome to the analyzer program #{first_name} #{last_name}"
+puts "What do you want to do? 1) multiply 2) divide 3) subtract 4) find remainder"
+prompt = gets.chomp
 
-puts "Your first name has #{first_name.length} characters in it"
-
-puts "Your last name has #{last_name.length} characters in it"
-
-full_name = first_name + " " + last_name
-
-puts "Your name in reverse reads #{full_name.reverse}"
-
-puts "Please enter your first number"
+puts "Enter a number"
 first_number = gets.chomp
-
-puts "Please enter your second number"
+puts "Enter in your second number"
 second_number = gets.chomp
 
-puts "The first number multiplied by the second number is: #{first_number.to_i * second_number.to_i}"
+if prompt == '1'
+  puts "You have chosen to multiply #{first_number} with #{second_number}"
+  result = multiply(first_number, second_number)
+elsif prompt == '2'
+  puts "You have chosen to divide #{first_number} with #{second_number}"
+  result = divide(first_number, second_number)
+elsif prompt == '3'
+  puts "You have chosen to subtract #{second_number} with #{first_number}"
+  result = subtract(first_number, second_number)
+elsif prompt == '4'
+  puts "You have chosen to find the remainder #{first_number} with #{second_number}"
+  result = mod(first_number, second_number)
+else
+  puts "Pick a number from 1-4 stupid"
+end
 
-puts "The first number divided by the second number is: #{first_number.to_f / second_number.to_f}"
+puts "The result is #{result}"
+  
 
-puts "The first number mod by the second number is: #{first_number.to_i % second_number.to_i}"
+#puts "Please enter your first number"
+#first_number = gets.chomp
+#puts "Please enter your second number"
+#second_number = gets.chomp
+
 
